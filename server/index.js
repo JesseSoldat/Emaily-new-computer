1de3e58eb91222cbd781154a68676c495f0468fe
Jesse <jsoldat@hotmail.com>
2018-02-28 13:56:59 +0700

    server

--- server/index.js
+++ server/index.js
@@ -1,2 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('../client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`SERVER running at ${PORT}`) );
